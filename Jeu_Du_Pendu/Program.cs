f9cc3ee22b0cbfe108b4d86db23ed4f3a0fc8d38
Koumeil Messaoudi <k.messaoudi@stag.technofuturtic.education>
2023-03-18 23:32:33 +0100

    ajout fichier  ascii class PENDU + afficher chéma quand vie--

--- Jeu_Du_Pendu/Program.cs
+++ Jeu_Du_Pendu/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Linq.Expressions;
+using AsciiArt;
 
 class Program
 {
@@ -16,12 +17,22 @@ class Program
             }
             else
             {
-                Console.Write("-");
+                Console.Write("- ");
             }
             
         }
         Console.WriteLine();
     }
+
+    static bool verifier_reponse(string mot, string reponse)
+    {   
+        if (mot == reponse)
+        {
+            return true;
+        }
+        return false;
+    }
+
     static char Demander_Lettre()
     {   
        
@@ -41,36 +52,67 @@ class Program
     static void Deviner_Mot(string mot)
     {
         var lettreDevinee = new List<char>();
+        var liste_lettre_fausse = new List<char>();
+        int index = 0;
+
         string bonneReponse = string.Empty;
         int vie = 6;
 
         while (vie > 0)
-        {
-            Afficher_Mot(mot, lettreDevinee);
-            var lettre = Demander_Lettre();
-            
-            Console.Clear();
+        {   
+
             Console.WriteLine($"Vie restante : {vie}");
+
+            Console.WriteLine(Ascii.PENDU[index]);
+
+            Console.WriteLine();
+
+            Console.Write("Lettre eronnées : " + String.Join(", ", liste_lettre_fausse));
+
+       
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Afficher_Mot(mot, lettreDevinee);
+            Console.WriteLine();
+            
+
+            var lettre = Demander_Lettre();
+
+            Console.WriteLine();
+
+            
+
+            Console.Clear();
+            
 
             if (mot.Contains(lettre))
             {
-                Console.WriteLine("Cette lettre est dans le mot");
-                
+                Console.WriteLine("Cool ! Cette lettre est dans le mot. ");
+                Console.WriteLine();
+
+
                 lettreDevinee.Add(lettre);
                 bonneReponse += lettre;
             }
             else
             {
-                Console.WriteLine("Cette lettre n'est pas dans le mot");
+                Console.WriteLine("Hélas! Cette lettre n'est pas dans le mot..");
+                Console.WriteLine();
+                liste_lettre_fausse.Add(lettre);
+                
                 vie--;
+                index++;
             }
+            bool verification = verifier_reponse(mot, bonneReponse);
 
-            if (mot == bonneReponse)
+            if(verification == true)
             {
                 Console.Clear();
                 Console.Write($"Vous avez gagné ! Le mot à deviner était {mot} !");
                 break;
             }
+
 
         }
 
